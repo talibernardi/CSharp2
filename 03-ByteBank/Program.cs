0581557a4c5126350bd9666c325aa0cde3c4aa7d
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-09-29 16:27:18 -0300

    03bytebank

---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 863452;
+
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+            contaDaGabriela.saldo = 300;
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDaGabrielaCosta.saldo);
+
+
+
+            if (contaDaGabriela.saldo >= 100)
+            {
+                contaDaGabriela.saldo -= 100;
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
