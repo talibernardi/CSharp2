c0bb08044861df8df114e14f9c814ceea54fd3c8
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-09-29 16:29:34 -0300

    07bytebank

---
+++ 07-ByteBank/Cliente.cs
@@ -0,0 +1,23 @@
+﻿namespace _07_ByteBank
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string _cpf
+        {
+            get
+            {
+                return _cpf;
+            }
+
+            set
+            {
+                // minha lógica aqui
+                _cpf = value;
+            }
+        }
+        public string Profissao { get; set; }
+    }
+
+}
+
