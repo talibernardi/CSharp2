4d9ba83820387c110d6ccdf063808e20de8b6bdb
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-09-29 16:27:47 -0300

    04bytebank

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+
+            Console.WriteLine(resultadoSaque);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
