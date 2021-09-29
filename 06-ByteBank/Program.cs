c08aff0dc1947d48f300600d14e047808299114e
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-09-29 16:28:42 -0300

    06bytebank

---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+
+            cliente.Nome = "Guilherme";
+            cliente.CPF = "434.564.879-20";
+            cliente.Profissao = "Desenvolvedor";
+
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.Write(conta.Saldo);
+
+            Console.ReadLine();
+
+        }
+    }
+}
