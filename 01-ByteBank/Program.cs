17e4f7751dd923fb77a8f02bc085545935f801c8
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-09-30 10:42:44 -0300

    04bytebank

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -1,28 +1,103 @@
 ﻿using System;
 
 namespace _01_ByteBank
-{
-    class Program
+{ 
+ class Program
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaDaGabriela = new ContaCorrente();
+            try
+            {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("CATCH NO METODO MAIN");
+            }
 
-            contaDaGabriela.titular = "Gabriela";
-            contaDaGabriela.agencia = 863;
-            contaDaGabriela.numero = 863452;
-            contaDaGabriela.saldo = 100;
-
-            Console.WriteLine(contaDaGabriela.titular);
-            Console.WriteLine("Agência " + contaDaGabriela.agencia);
-            Console.WriteLine("Número " + contaDaGabriela.numero);
-            Console.WriteLine("Saldo " + contaDaGabriela.saldo);
-
-            contaDaGabriela.saldo += 200;
-            Console.WriteLine("Saldo " + contaDaGabriela.saldo);
-
-
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
             Console.ReadLine();
         }
+
+        private static void CarregarContas()
+        {
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+
+
+            // ---------------------------------------------
+
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contasl.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally");
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
+                ContaCorrente conta1 = new ContaCorrente(4564, 789684);
+                ContaCorrente conta2 = new ContaCorrente(7891, 456794);
+
+                // conta1.Transferir(10000, conta2);
+                conta1.Sacar(10000);
+            }
+            catch (OperacaoFinanceiraException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+
+                // Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
+
+            }
+        }
+
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
+                throw;
+                Console.WriteLine("Código depois do throw");
+            }
+        }
+
+        // numero = 1
+        // divisor = 2;
+
     }
 }
