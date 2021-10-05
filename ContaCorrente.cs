eb49b5dfdd5eae94311e5454535047d7f96d3f9f
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-10-05 10:14:27 -0300

    2commit

---
+++ ContaCorrente.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ByteBank
+
+
+{
+    namespace ByteBank
+        {
+        public class ContaCorrente
+        {
+            private static int TaxaOperacao;
+
+            public static int TotalDeContasCriadas { get; private set; }
+
+            public Cliente Titular { get; set; }
+
+            public int Numero { get; }
+            public int Agencia { get; }
+
+            private double _saldo = 100;
+            public double Saldo
+            {
+                get
+                {
+                    return _saldo;
+                }
+                set
+                {
+                    if (value < 0)
+                    {
+                        return;
+                    }
+
+                    _saldo = value;
+                }
+            }
+
+            public ContaCorrente(int agencia, int numero)
+            {
+                if (numero <= 0)
+                {
+                    throw new ArgumentException("O argumento agencia deve ser maior que 0.", nameof(agencia));
+                }
+
+                if (numero <= 0)
+                {
+                    throw new ArgumentException("O argumento numero deve ser maior que 0.", nameof(numero));
+                }
+
+                Agencia = agencia;
+                Numero = numero;
+
+                TotalDeContasCriadas++;
+                TaxaOperacao = 30 / TotalDeContasCriadas;
+            }
+
+            public void Sacar(double valor)
+            {
+                if (_saldo < valor)
+                {     
+                    throw new SaldoInsuficienteException("Saldo insuficiente para o saque no valor de " + valor);
+                }
+
+                _saldo -= valor;
+               
+            }
+
+            public void Depositar(double valor)
+            {
+                _saldo += valor;
+            }
+
+            public bool Transferir(double valor, ContaCorrente contaDestino)
+            {
+                if (_saldo < valor)
+                {
+                    return false;
+                }
+
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
